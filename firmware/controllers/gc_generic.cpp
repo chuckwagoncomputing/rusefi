cb9fbd38b4fc1777f356188e4657a4b05bde96f0
David Holdeman <dave@holdemanenterprises.com>
2022-04-29 17:17:29 -0500

    use for loop

--- firmware/controllers/gc_generic.cpp
+++ firmware/controllers/gc_generic.cpp
@@ -9,23 +9,10 @@ GenericGearController::GenericGearController() {
 }
 
 void GenericGearController::init() {
-	if (engineConfiguration->tcu_rangeInput[1] != GPIO_UNASSIGNED) {
-		efiSetPadMode("Range Input 1", engineConfiguration->tcu_rangeInput[1], getInputMode(engineConfiguration->tcu_rangeInputMode[1]));
-	}
-	if (engineConfiguration->tcu_rangeInput[2] != GPIO_UNASSIGNED) {
-		efiSetPadMode("Range Input 2", engineConfiguration->tcu_rangeInput[2], getInputMode(engineConfiguration->tcu_rangeInputMode[2]));
-	}
-	if (engineConfiguration->tcu_rangeInput[3] != GPIO_UNASSIGNED) {
-		efiSetPadMode("Range Input 3", engineConfiguration->tcu_rangeInput[3], getInputMode(engineConfiguration->tcu_rangeInputMode[3]));
-	}
-	if (engineConfiguration->tcu_rangeInput[4] != GPIO_UNASSIGNED) {
-		efiSetPadMode("Range Input 4", engineConfiguration->tcu_rangeInput[4], getInputMode(engineConfiguration->tcu_rangeInputMode[4]));
-	}
-	if (engineConfiguration->tcu_rangeInput[5] != GPIO_UNASSIGNED) {
-		efiSetPadMode("Range Input 5", engineConfiguration->tcu_rangeInput[5], getInputMode(engineConfiguration->tcu_rangeInputMode[5]));
-	}
-	if (engineConfiguration->tcu_rangeInput[6] != GPIO_UNASSIGNED) {
-		efiSetPadMode("Range Input 6", engineConfiguration->tcu_rangeInput[6], getInputMode(engineConfiguration->tcu_rangeInputMode[6]));
+	for (int i = 0; i < 6; i++) {
+		if (engineConfiguration->tcu_rangeInput[i] != GPIO_UNASSIGNED) {
+			efiSetPadMode("Range Input", engineConfiguration->tcu_rangeInput[i], getInputMode(engineConfiguration->tcu_rangeInputMode[i]));
+		}
 	}
 
 	GearControllerBase::init();
