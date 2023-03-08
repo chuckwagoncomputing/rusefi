dc9bbada20ecc46d674987e717847e61c7ad08e4
David Holdeman <dave@holdemanenterprises.com>
2023-03-07 21:35:28 -0600

    change check

--- misc/jenkins/InteractiveHtmlBom/run.sh
+++ misc/jenkins/InteractiveHtmlBom/run.sh
@@ -4,8 +4,8 @@ INTERACTIVE_HTML_BOM_NO_DISPLAY="true"
 IBOM_CMD="python3 hardware/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --name-format \"%f_latest\" --dest-dir ../ibom --include-nets"
 echo "IBOM_CMD=$IBOM_CMD"
 
-pwd
-[ -e hardware/frankenso/frankenso.kicad_pcb ] || { echo "hardware/frankenso/frankenso.kicad_pcb not found. Was this invoked from wrong folder?"; exit -1; }
+echo "Current directory: $(pwd)"
+[ $(basename $(pwd)) = 'rusef' ] || { echo "Not in 'rusefi' directory. Was this invoked from the wrong directory?"; exit 1; }
 
 for f in $(ls hardware/*/*.kicad_pcb); do
   if ls $(dirname $f)/$(basename $f .kicad_pcb).net 2>/dev/null; then
