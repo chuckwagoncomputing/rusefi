6b49dba5c84af04398741a23eaa952a696fd0997
David Holdeman <dave@holdemanenterprises.com>
2023-03-11 11:56:41 -0600

    use single quotes

--- misc/jenkins/InteractiveHtmlBom/run.sh
+++ misc/jenkins/InteractiveHtmlBom/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INTERACTIVE_HTML_BOM_NO_DISPLAY="true"
-IBOM_CMD="python3 hardware/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --name-format \"%f_latest\" --dest-dir ../ibom --include-nets"
+IBOM_CMD='python3 hardware/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --name-format "%f_latest" --dest-dir ../ibom --include-nets'
 echo "IBOM_CMD=$IBOM_CMD"
 
 echo "Current directory: $(pwd)"
