d5e6886a7b2893c298e9e87c6afa86ba531d198e
David Holdeman <dave@holdemanenterprises.com>
2026-02-09 06:32:40 +0000

    Build trigger_type_e_enum from triggers.yaml

--- java_tools/trigger-image/src/main/java/com/rusefi/trigger/TriggerWheelInfo.java
+++ java_tools/trigger-image/src/main/java/com/rusefi/trigger/TriggerWheelInfo.java
@@ -479,6 +479,10 @@ public class TriggerWheelInfo {
 
     public boolean isShapeWithoutTdc() {
         return shapeWithoutTdc;
+		}
+
+		public boolean isHidden() {
+        return hidden;
     }
 
     public TriggerGaps getGaps() {
